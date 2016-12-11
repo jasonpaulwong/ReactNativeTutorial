e7b731ca32fd42aa202e7012e280527fbbca82f6
Jason Wong <Jason@Jasons-MBP.san.rr.com>
2016-12-10 22:54:42 -0800

    Finished

--- SearchPage.js
+++ SearchPage.js
@@ -112,7 +112,7 @@ class SearchPage extends Component {
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
-        <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+        <TouchableHighlight style={styles.button} underlayColor='#99d9f4' onPress={this.onLocationPressed.bind(this)}>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('./Resources/house.png')} style={styles.image}/>
@@ -131,8 +131,23 @@ class SearchPage extends Component {
     this._executeQuery(query);
   }
 
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({searchString: search});
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+        });
+      }
+    )
+  }
+
   _executeQuery(query) {
-    console.log(query);
     this.setState({ isLoading: true });
 
     fetch(query)
