62a2613811ca717b5a8e3194264d072f0b2ece7b
Jason Wong <Jason@Jasons-MBP.san.rr.com>
2016-12-10 21:16:32 -0800

    Finished making buttons and making them do somthing

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
 
 var styles = ReactNative.StyleSheet.create({
   text: {
@@ -12,12 +13,36 @@ var styles = ReactNative.StyleSheet.create({
   }
 });
 
-class PropertyFinderApp extends React.Component {
+class HelloWorld extends React.Component {
   render() {
-    return React.createElement(ReactNative.Text, {style: styles.text},
-      "Hello World");
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
   }
 }
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
 
 ReactNative.AppRegistry.registerComponent('PropertyFinder', function() {
   return PropertyFinderApp;
